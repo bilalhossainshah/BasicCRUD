5989323b7b223091ade19e0c320235c2a1b32d08
myrepo <iambilalshah007@gmail.com>
2025-10-31 23:16:06 +0500

    added complete auth module

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 from database import engine
 import models
-from routers import products,categorey
+from routers import products,categorey,auth
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind = engine)
 app.include_router(products.router)
 app.include_router(categorey.router)
+app.include_router(auth.router)
 
--- models.py
+++ models.py
@@ -1,6 +1,15 @@
 from sqlalchemy import String,Column,Integer,ForeignKey
 from database import Base
 from sqlalchemy.orm import relationship
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+
+
 class Products(Base):
     __tablename__ = "Products"
     id = Column(Integer, primary_key=True, index=True)
